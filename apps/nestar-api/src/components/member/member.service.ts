e3a67761baf084550ad41fa8d228f1468e0cab20
murodjon-hub <murodjon.woosong@gmail.com>
2025-12-31 13:08:57 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -18,8 +18,8 @@ export class MemberService {
         //TODO: Authentication via tokens
         return result;
         } catch(err) {
-        console.log("Error, Service.model:", err);
-        throw new BadRequestException(err);
+        console.log("Error, Service.model:", err.message);
+        throw new BadRequestException(Message.USED_MEMBER_NICK_OR_NICK);
         }
 
     } 
--- apps/nestar-api/src/libs/enums/common.enum.ts
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -6,7 +6,8 @@ export enum Message {
   REMOVE_FAILED = 'Remove failed!',
   UPLOAD_FAILED = 'Upload failed!',
   BAD_REQUEST = 'Bad Request!',
-
+ 
+  USED_MEMBER_NICK_OR_NICK = "Already used member nick or phone!",
   NO_MEMBER_NICK = 'No member with that member nick!',
   BLOCKED_USER = 'You have been blocked!',
   WRONG_PASSWORD = 'Wrong password, try again!',
