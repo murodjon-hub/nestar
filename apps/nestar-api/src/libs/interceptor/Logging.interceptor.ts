8ce395c4c5eeaf3a8b948ff9a09857e99a855dc2
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-01 12:28:01 +0900

    feat: hashing password via bcryptjs

--- apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -17,16 +17,17 @@ export class LoggingInterceptor implements NestInterceptor {
    
 
     if(requestType === "http") {
+        return next.handle().pipe();
     // DEVELOP IF NEEDED!
     } else if(requestType === "graphql") {
-        /** (1) Print Request */
+        /* (1) Print Request */
     const gqlContext = GqlExecutionContext.create(context);
     this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, "REQUEST");
 
 
-      /**(2) Errors handling via GraphQL */
+      /*(2) Errors handling via GraphQL */
 
-      /**(3) No Errors, giving Response below */
+      /*(3) No Errors, giving Response below */
      return next.handle().pipe(
       tap((context) => { 
         const responseTime = Date.now() - recordTime;
