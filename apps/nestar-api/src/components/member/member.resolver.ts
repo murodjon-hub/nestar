4da98ea5f04faf447ffc1c894dff5e9a8f3129f8
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-04 23:37:13 +0900

    feat: dvelop get agent GraphQl api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,8 +1,8 @@
 import { Mutation, Resolver, Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { UseGuards} from '@nestjs/common';
-import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
-import { Member } from '../../libs/dto/member/member';
+import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { Member, Members } from '../../libs/dto/member/member';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
@@ -69,6 +69,15 @@ export class MemberResolver {
         const targetId =  shapeIntoMongoObjectId(input);
         return this.memberService.getMember(memberId, targetId);
     }
+
+
+    @UseGuards(WithoutGuard)
+    @Query(() => Members)
+    public async getAgents(@Args("input") input: AgentsInquiry, @AuthMember("_id") memberId: ObjectId): Promise<string> {
+        console.log("Query: getAgents");
+    return this.memberService.getAgents(memberId, input);
+    }
+
 
     /**ADMIN */
     @Roles(MemberType.ADMIN)
--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -85,3 +85,18 @@ export class Member {
     accessToken?: string;
 
 }
+
+@ObjectType()
+export class TotalCounter {
+    @Field(() => Int, {nullable: true})
+    total: number;
+}
+
+@ObjectType()
+export class Members {
+    @Field(() => [Member])
+    list: Member[];
+
+    @Field(() => [TotalCounter], {nullable: true})
+    metaCounter: TotalCounter[];
+}
