be165b568c322db41a7c286f883b1c1f3b01e5e7
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-08 14:11:14 +0900

    feat: develop updateProperty business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -10,32 +10,46 @@ import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
-    constructor(private readonly propertyService: PropertyService) {}
+	constructor(private readonly propertyService: PropertyService) {}
 
-@Roles(MemberType.AGENT)
-@UseGuards(RolesGuard)
-@Mutation(() => Property) 
-public async createProperty(
-    @Args("input") input: PropertyInput, 
-    @AuthMember("_id") memberId: ObjectId): 
-    Promise<Property> {
-    console.log("Mutation: createProperty");
-    input.memberId = memberId;
+	@Roles(MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Property)
+	public async createProperty(
+		@Args('input') input: PropertyInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Mutation: createProperty');
+		input.memberId = memberId;
 
-    return await this.propertyService.createProperty(input);
-}
+		return await this.propertyService.createProperty(input);
+	}
 
-@UseGuards(WithoutGuard)
-@Query((returnS) => Property)
-public async getProperty(
-    @Args("propertyId") input: string,
-    @AuthMember("_id") memberId: ObjectId,
-): Promise<Property> {
-    console.log("Query: getProperty");
-    const propertyId = shapeIntoMongoObjectId(input);
-    return await this.propertyService.getProperty(memberId, propertyId);
+	@UseGuards(WithoutGuard)
+	@Query((returnS) => Property)
+	public async getProperty(
+		@Args('propertyId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Property> {
+		console.log('Query: getProperty');
+		const propertyId = shapeIntoMongoObjectId(input);
+		return await this.propertyService.getProperty(memberId, propertyId);
+	}
+
+    @Roles(MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => Property)
+    public async updateProperty(
+        @Args('input') input: PropertyUpdate,
+		@AuthMember('_id') memberId: ObjectId,
+    ): Promise<Property> {
+        console.log("Mutation: updateProperty");
+        input._id = shapeIntoMongoObjectId(input._id);
+        return await this.propertyService.updateProperty(memberId, input);
+    }
 }
-}
+ 
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -9,6 +9,8 @@ import { ViewService } from '../view/view.service';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import moment from 'moment';
 
 @Injectable()
 export class PropertyService {
@@ -35,39 +37,65 @@ export class PropertyService {
 		}
 	}
 
-  public async getProperty( memberId: ObjectId, propertyId: ObjectId): Promise<Property> {
-    const search = {
-  _id: propertyId,
-  propertyStatus: PropertyStatus.ACTIVE, 
-};
+	public async getProperty(memberId: ObjectId, propertyId: ObjectId): Promise<Property> {
+		const search = {
+			_id: propertyId,
+			propertyStatus: PropertyStatus.ACTIVE,
+		};
 
+		const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
+		if (!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-    const targetProperty: Property = await this.propertyModel.findOne(search).lean().exec();
-    if(!targetProperty) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		if (memberId) {
+			const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY };
+			const newView = await this.viewService.recordView(viewInput);
+			if (newView) {
+				await this.propertyStatsEditor({ _id: propertyId, targetKey: 'propertyViews', modifier: 1 });
+				targetProperty.propertyViews++;
+			}
 
-    if(memberId) {
-      const viewInput = { memberId: memberId, viewRefId: propertyId, viewGroup: ViewGroup.PROPERTY};
-      const newView = await this.viewService.recordView(viewInput);
-      if (newView) {
-        await this.propertyStatsEditor({ _id: propertyId, targetKey: "propertyViews", modifier: 1});
-        targetProperty.propertyViews++;
-      }
+			//meLiked
+		}
+		targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
+		return targetProperty;
+	}
 
-      //meLiked
-    }
-    targetProperty.memberData = await this.memberService.getMember(null, targetProperty.memberId);
-    return targetProperty;
-  }
+	public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
+		const { _id, targetKey, modifier } = input;
+		return await this.propertyModel
+			.findByIdAndUpdate(
+				_id,
+				{ $inc: { [targetKey]: modifier } },
+				{
+					new: true,
+				},
+			)
+			.exec();
+	}
+	public async updateProperty(memberId: ObjectId, input: PropertyUpdate): Promise<Property> {
+		let { propertyStatus, soldAt, deletedAt } = input;
+		const search: T = {
+			_id: input._id,
+			memberId: memberId,
+			propertyStatus: PropertyStatus.ACTIVE,
+		};
+		if (propertyStatus === PropertyStatus.SOLD) soldAt = moment().toDate();
+		else if (propertyStatus === PropertyStatus.DELETE) deletedAt = moment().toDate();
 
-  public async propertyStatsEditor(input: StatisticModifier): Promise<Property> {
-    const { _id, targetKey, modifier } = input;
-    return await this.propertyModel.findByIdAndUpdate(
-      _id,
-      {$inc: {[targetKey]: modifier}},
-      {
-        new: true,
-      },
-    )
-    .exec();
-  }
+		const result = await this.propertyModel
+			.findOneAndUpdate(search, input, {
+				new: true,
+			})
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		if (soldAt || deletedAt) {
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberProperties',
+				modifier: -1,
+			});
+		}
+		return result;
+	}
 }
