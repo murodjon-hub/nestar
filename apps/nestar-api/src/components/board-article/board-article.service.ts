99b425fdf6341c6b6dd3e982f938c3f678195c81
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-13 21:49:44 +0900

    feat: develop admin related boardArticle GraphQL API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -4,7 +4,11 @@ import { Model, ObjectId } from 'mongoose';
 import { BoardArticle, BoardArticles } from '../../libs/dto/board-article/board-article';
 import { MemberService } from '../member/member.service';
 import { ViewService } from '../view/view.service';
-import { BoardArticleInput, BoardArticlesInquiry } from '../../libs/dto/board-article/board-article.input';
+import {
+	AllBoardArticlesInquiry,
+	BoardArticleInput,
+	BoardArticlesInquiry,
+} from '../../libs/dto/board-article/board-article.input';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { BoardArticleStatus } from '../../libs/enums/board-article.enum';
@@ -82,44 +86,43 @@ export class BoardArticleService {
 
 		return result;
 	}
-    
 
-    public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): Promise<BoardArticles> {
-    const { articleCategory, text } = input.search;
-    const match: T = { articleStatus: BoardArticleStatus.ACTIVE };
-    const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+	public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): Promise<BoardArticles> {
+		const { articleCategory, text } = input.search;
+		const match: T = { articleStatus: BoardArticleStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
 
-    if (articleCategory) match.articleCategory = articleCategory;
-    if (text) match.articleTitle = { $regex: new RegExp(text, 'i') };
-    if (input.search?.memberId) {
-        match.memberId = shapeIntoMongoObjectId(input.search.memberId);
-    }
+		if (articleCategory) match.articleCategory = articleCategory;
+		if (text) match.articleTitle = { $regex: new RegExp(text, 'i') };
+		if (input.search?.memberId) {
+			match.memberId = shapeIntoMongoObjectId(input.search.memberId);
+		}
 
-    console.log('match:', match);
+		console.log('match:', match);
 
-    const result = await this.boardArticleModel
-        .aggregate([
-            { $match: match },
-            { $sort: sort },
-            {
-                $facet: {
-                    list: [
-                        { $skip: (input.page - 1) * input.limit },
-                        { $limit: input.limit },
-                        // meLiked
-                        lookupMember,
-                        { $unwind: '$memberData' },
-                    ],
-                    metaCounter: [{ $count: 'total' }],
-                },
-            },
-        ])
-        .exec();
+		const result = await this.boardArticleModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							// meLiked
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
 
-    if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 
-    return result[0];
-}
+		return result[0];
+	}
 
 	//STATISTICS
 	public async boardArticleStatsEditor(input: StatisticModifier): Promise<BoardArticle> {
@@ -133,5 +136,68 @@ export class BoardArticleService {
 				},
 			)
 			.exec();
+	}
+
+	/**ADMIN **/
+	public async getAllBoardArticlesByAdmin(input: AllBoardArticlesInquiry): Promise<BoardArticles> {
+		const { articleStatus, articleCategory } = input.search;
+		const match: T = {};
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+
+		if (articleStatus) match.articleStatus = articleStatus;
+		if (articleCategory) match.articleCategory = articleCategory;
+
+		const result = await this.boardArticleModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: sort },
+				{
+					$facet: {
+						list: [
+							{ $skip: (input.page - 1) * input.limit },
+							{ $limit: input.limit },
+							lookupMember,
+							{ $unwind: '$memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+		return result[0];
+	}
+
+	public async updateBoardArticleByAdmin(input: BoardArticleUpdate): Promise<BoardArticle> {
+		const { _id, articleStatus } = input;
+
+		const result = await this.boardArticleModel
+			.findOneAndUpdate({ _id: _id, articleStatus: BoardArticleStatus.ACTIVE }, input, {
+				new: true,
+			})
+			.exec();
+
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		if (articleStatus === BoardArticleStatus.DELETE) {
+			await this.memberService.memberStatsEditor({
+				_id: result.memberId,
+				targetKey: 'memberArticles',
+				modifier: -1,
+			});
+		}
+
+		return result;
+	}
+    
+	public async removeBoardArticleByAdmin(articleId: ObjectId): Promise<BoardArticle> {
+		const search: T = { _id: articleId, articleStatus: BoardArticleStatus.DELETE };
+		const result = await this.boardArticleModel.findOneAndDelete(search).exec();
+
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
 	}
 }
--- apps/nestar-api/src/libs/dto/board-article/board-article.input.ts
+++ apps/nestar-api/src/libs/dto/board-article/board-article.input.ts
@@ -3,6 +3,7 @@ import { IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
 import { ObjectId } from 'mongoose';
 import { BoardArticleCategory, BoardArticleStatus } from '../../enums/board-article.enum';
 import { Direction } from '../../enums/common.enum';
+import { availableBoardArticleSorts } from '../../config';
 
 
 @InputType()
@@ -56,7 +57,7 @@ export class BoardArticlesInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn(['createdAt', 'updatedAt', 'articleLikes', 'articleViews'])
+	@IsIn(availableBoardArticleSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
@@ -93,7 +94,7 @@ export class AllBoardArticlesInquiry {
 	limit: number;
 
 	@IsOptional()
-	@IsIn(['createdAt', 'updatedAt', 'articleLikes', 'articleViews'])
+	@IsIn(availableBoardArticleSorts)
 	@Field(() => String, { nullable: true })
 	sort?: string;
 
