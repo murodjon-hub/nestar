59398c83329562246a6de6bbdd2946b727e31c4b
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-13 23:16:26 +0900

    feat: configure comment module and create comment related service model

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -18,6 +18,7 @@ import { MemberModule } from '../member/member.module';
           MemberModule,
         ],
   providers: [PropertyResolver, PropertyService],
+  exports: [PropertyService],
 
 })
 export class PropertyModule {}
