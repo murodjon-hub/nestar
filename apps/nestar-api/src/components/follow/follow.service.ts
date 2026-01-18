6e2bf95e5f654ca6ee5445c8650f8e39d0eb98c4
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-18 22:03:17 +0900

    fix: integrate of lookupAuthMemberFollowed business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -4,7 +4,12 @@ import { Follower, Followers, Following, Followings } from '../../libs/dto/follo
 import { Model, ObjectId } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import {
+	lookupAuthMemberFollowed,
+	lookupAuthMemberLiked,
+	lookupFollowerData,
+	lookupFollowingData,
+} from '../../libs/config';
 import { T } from '../../libs/types/common';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 
@@ -75,8 +80,8 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							lookupAuthMemberLiked(memberId, "$followingId"),
-							// meFollowed
+							lookupAuthMemberLiked(memberId, '$followingId'),
+							lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId' }),
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
 						],
@@ -107,8 +112,9 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							lookupAuthMemberLiked(memberId, "$followerId"),
-							// meFollowed
+							lookupAuthMemberLiked(memberId, '$followerId'),
+							lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId' }),
+							lookupFollowingData,
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
 						],
