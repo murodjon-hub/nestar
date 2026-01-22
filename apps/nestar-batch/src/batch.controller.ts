c4147fad07b99b2b115babd80f9b0eb2592369d8
murodjon-hub <murodjon.woosong@gmail.com>
2026-01-22 22:29:57 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-  constructor(private readonly nestarBatchService: BatchService) {}
+	constructor(private readonly BatchService: BatchService) {}
 
-  @Get()
-  getHello(): string {
-    return this.nestarBatchService.getHello();
+	private logger: Logger = new Logger('BatchController');
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY!');
+	}
+
+	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	public async batchRollback() {
+		try {
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchRollback();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchProperties() {
+		try {
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchProperties();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	public async batchAgents() {
+		try {
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED!');
+			await this.BatchService.batchAgents();
+		} catch (err) {
+			this.logger.error(err);
+		}
+	}
+
+	/*
+  @Interval(1000)
+	handleInterval() {
+    this.logger.debug('INTERVAL TEST');
   }
+  */
+
+	@Get()
+	getHello(): string {
+		return this.BatchService.getHello();
+	}
 }
